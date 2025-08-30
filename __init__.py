0aa927d4dc5e0c0abd5437cdfeb2704b029b4c2a
Minepotato <enzo.beheshti@gmail.com>
2025-08-30 19:52:38 +0200

    Ajout des fichiers __init__.py et breakfast.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# Importation de la classe Breakfast pour qu'elle soit accessible directement depuis le package
+from .breakfast import Breakfast
+
+# Tu peux aussi initialiser d'autres choses ici, si nécessaire
+print("Breakfast package loaded successfully!")
